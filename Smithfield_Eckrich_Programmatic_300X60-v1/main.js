afa47d004c17649fb3f9a25f247fb8c43391e478
Mahesh Bhatt <141926871+adamandeveddbstudios@users.noreply.github.com>
2024-08-20 15:49:34 +0530

    Add files via upload

--- Smithfield_Eckrich_Programmatic_300X60-v1/main.js
+++ Smithfield_Eckrich_Programmatic_300X60-v1/main.js
@@ -18,7 +18,7 @@ function init() {
 function animate() {
   tl.set(["#main_content"], { autoAlpha: 1, rotationZ: 0.1, force3D: true });
   tl.set(["#photo, #photo2"], { rotationZ: 0.1, force3D: true });
-  tl.set(["#photo2"], { x: 160 });
+  tl.set(["#photo2"], { x: 160,  autoAlpha:1 });
 
   tl.addLabel('frame_1', 0)
     .to(['#logo'], 0.5, { autoAlpha: 1, ease: Power1.easeInOut }, "frame_1")
